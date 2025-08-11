577f46f1a14d37b580164444ab2e441766c40833
Esha Patel <Patellesha11@gmail.com>
2025-08-11 12:52:34 +0300

    added

--- backend/config.py
+++ backend/config.py
@@ -3,5 +3,5 @@ import os
 class Config:
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "postgresql://postgres:3031@localhost/taskadee")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = 'your-secret-key'
-    JWT_SECRET_KEY = 'your-jwt-secret-key'
+    SECRET_KEY = 'Dfl3!s#e9rC^j2KP)w^N+z3L0wqvXbR@u$Gm4rXt6qLv' 
+    JWT_SECRET_KEY = 'pzQ6xK$L#B@uE2!cN*wYvX1Hr7pTgZq8U%R&v0fM#bKs'
